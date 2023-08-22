62aeafa1a81397c63641166304bea10b04f00447
DalChongo <chabechongo@hotmail.com>
2023-08-22 15:40:46 -0400

    adding bash file with ansible commands

---
+++ ansible_commands.sh
@@ -0,0 +1,2 @@
+# this is the command to ping the machines controlled by the master in ansible
+# ansible -i all [inventory name w/ extension] [name of groups previously specify inside inventory] -m ping
