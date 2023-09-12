889c7713d5334c82038fbe6e4f86461b696df744
DalChongo <chabechongo@hotmail.com>
2023-09-11 23:07:40 -0400

    last modification file

--- ansible_commands.sh
+++ ansible_commands.sh
@@ -1,2 +1,4 @@
 # this is the command to ping the machines controlled by the master in ansible
 # ansible -i all [inventory name w/ extension] [name of groups previously specify inside inventory] -m ping
+# - name: Ping my hosts
+#      ansible.builtin.package: *command if i want to intall a package from a playbook*
